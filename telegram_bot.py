3daf842a1b283904ff9158be48927382f8ae0cd3
arturha4 <sadbooys.2001@gmail.com>
2020-10-26 13:05:52 +0500

    Добавлены кнопки, тесты, обработка исключений

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,23 +1,47 @@
-# import telebot
-#
-# token = '1260767500:AAFq0jjuZ6isvMj4OPn8yizFRDmR8yG4Glc'
-# bot = telebot.TeleBot(token)
-# #1. получаем id делаем запрос к вк api, делаем ф-ию, котрая считает время в сети
-# #2. бот отправлял ссылку на страницу, чтобы проверить верность введённого id, если все верно, ответ кнопка да или нет
-# #3. пробовать получить id не ручным вводом
-# @bot.message_handler(commands=['start'])
-#
-#
-#
-# def start_message(message):
-#     bot.reply_to(message,
-#                  "Привет, я бот для учета времени, я помогу тебе отслеживать твою активность в социальных сетях и не только."
-#                  " Пока что я работаю только с вк,  введи свой id vk или ник, который находится в адресной строке")
-#
-#
-# @bot.message_handler(func=lambda message: True)
-# def get_id_vk(message):
-#     print(message.text)
-#
-#
-# bot.polling(none_stop=True)
+import telebot
+from telebot import types
+from vk_api import create_user
+token = '1260767500:AAFq0jjuZ6isvMj4OPn8yizFRDmR8yG4Glc'
+bot = telebot.TeleBot(token)
+
+
+#send id to function
+# 3. пробовать получить id не ручным вводом
+#4.чтобы возращало id, для того чтобы передать в бд
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.reply_to(message,
+                 "Привет, я бот для учета времени, я помогу тебе отслеживать твою активность в социальных сетях и не только."
+                 " Пока что я работаю только с вк,  введи свой id vk или ник, который находится в адресной строке")
+    bot.register_next_step_handler(message,url)
+
+
+@bot.message_handler(content_types=['text'])
+def url(message):
+    id=message.text
+    markup = types.InlineKeyboardMarkup()
+    btn_vk_page= types.InlineKeyboardButton(text='Перейти', url=f'https://vk.com/{id}')
+    btn_yes=types.InlineKeyboardButton(text='Да',callback_data='Yes')
+    btn_no=types.InlineKeyboardButton(text='Нет',callback_data='No')
+    markup.add(btn_vk_page,btn_yes,btn_no)
+    bot.send_message(message.chat.id, f"Твоя страница?https://vk.com/{id} ", reply_markup = markup)
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def callback_vk_verify(call):
+    try:
+        if call.data=='Yes':
+            bot.send_message(call.message.chat.id,'Отлично!')
+            create_user(call.message.text[call.message.text.find('?')-1:])
+        elif call.data=='No':
+            bot.send_message(call.message.chat.id,'Проверь ник ещё раз, если что-то не так введи данные ещё раз')
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id,text='Ок!',
+                              reply_markup=None)
+    except Exception as e:
+        print(repr(e))
+
+
+
+bot.polling(none_stop=True)
+
+
--- vk_api.py
+++ vk_api.py
@@ -4,7 +4,8 @@ vktoken = "46e50d8c46e50d8c46e50d8c8a46917fc2446e546e50d8c196fd185478ce57d33254a
 users=[]
 session = vk.Session()
 vkapi = vk.API(session)
-#добавить тесты
+#сделать функцию трекающую фремя vk
+#реализовать бд и работу с ней
 def create_user(id):
     data=vkapi.users.get(v=5.124, user_ids=str(id), fields='online, last_seen', access_token=vktoken)[0]
     online=data['online']
@@ -12,6 +13,11 @@ def create_user(id):
         users.append({'online':True,'id':id,'time_in_vk':0})
     else:
         users.append({'online':False,'id':id,'time_in_vk':0})
+    print(users)
+
+
+def track(id):
+    data=vkapi.users.get(v=5.124, user_ids=str(id), fields='online, last_seen', access_token=vktoken)[0]
 
 
 
